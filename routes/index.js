aa31e588ecbe9e70f6079a8b03b06d002fe4542a
MaxJevon <32112163+cryptorich1@users.noreply.github.com>
2023-09-04 23:34:58 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const AppController = require('../controllers/AppController');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
+const FilesController = require('../controllers/FilesController');
 
-// Define the endpoints
+
+//endpoints
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
-//new endpoint for creating a user
+router.post('/files', FilesController.postUpload);
 router.post('/users', UsersController.postNew);
 
 module.exports = router;
