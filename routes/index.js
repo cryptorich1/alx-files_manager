7bb725c30c7a39548150ed968485738d598932db
MaxJevon <32112163+cryptorich1@users.noreply.github.com>
2023-09-04 23:18:00 +0100

    new API end point

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const AppController = require('../controllers/AppController');
+const router = express.Router();
 const UserController = require('../controllers/UserController');
 
-
-const router = express.Router();
+// Define the endpoints
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 
 //new endpoint for creating a user
 router.post('/users', UsersController.postNew);
