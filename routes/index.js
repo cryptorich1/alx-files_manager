c7baa98df53926e1f146956f1711b2320fd2a165
MaxJevon <32112163+cryptorich1@users.noreply.github.com>
2023-09-05 17:49:46 +0100

    Update with put call

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,8 @@ router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/files', FilesController.postUpload);
 router.post('/users', UsersController.postNew);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
 
 module.exports = router;
